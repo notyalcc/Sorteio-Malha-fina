ab4589fd513acde4998b3388d147cfa785f2f104
notyalcc <CCLAYYTONNSS@GMAIL.COM>
2026-05-26 09:06:29 -0300

    Update admin and user passwords to 'admin'

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ else:
     app = Flask(__name__)
 
 app.secret_key = "chave_secreta_segura" # Necessário para o login funcionar
-SENHA_ADMIN = "82465123"#DFINA SUA SENHA DE ADMIN AQUI
-SENHA_USUARIO = "via@clayton" # Senha para acessar o sorteio (Operadores)
+SENHA_ADMIN = "admin"#DFINA SUA SENHA DE ADMIN AQUI
+SENHA_USUARIO = "admin" # Senha para acessar o sorteio (Operadores)
 
 PALAVRAS_MALHA = ['VAI PARA MALHA', 'SEGUIR PARA MALHA', 'VOCÊ MALHOU']
 PALAVRAS_LIBERADO = ['CARRO LIBERADO', 'PODE SEGUIR', 'AUTORIZADO']
